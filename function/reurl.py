6b4d686d3faec686ea8b82f8143802d3fd98f59c
陳勝舢 <54sakuramiku@gmail.com>
2019-09-22 18:12:00 +0800

    add code

--- function/reurl.py
+++ function/reurl.py
@@ -7,7 +7,7 @@ def reurl(url):
     data = {"url": url}
     headers = {'Content-type': 'application/json', 'reurl-api-key': RE_api_key}
     response = requests.post(
-        "https://api.reurl.cc/shorten", data=data_json, headers=headers)
+        "https://api.reurl.cc/shorten", data=data, headers=headers)
     s = response.json()
     print(s['short_url'])
     return s['short_url']
